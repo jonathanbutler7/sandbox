64ea7435c46c7ba119e5d23fc38d35c7f3ac92c1
Jonathan Butler <jonathanbutler7@gmail.com>
2023-03-02 12:28:49 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Text, ThemeProvider, useHoverLabel } from "@weave/design-system";
+import { theme } from "@weave/theme";
 
 function App() {
+  const { HoverLabel, labelProps, triggerProps } = useHoverLabel({
+    placement: "top",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider includeEmotionTheme>
+      <Text
+        weight="bold"
+        {...triggerProps}
+        style={{ display: "inline-block", color: theme.colors.primary80 }}
+      >
+        Hover to see label
+      </Text>
+      <HoverLabel {...labelProps}>I am a hover label</HoverLabel>
+    </ThemeProvider>
   );
 }
 
